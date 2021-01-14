147c73d3f546ceb2c738145604456e6c24b7afe5
Yusuf <yusufyilmazfr@gmail.com>
2021-01-14 21:15:31 +0300

    I created entities

---
+++ src/EnglishAssistantTelegramBot.Console/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishAssistantTelegramBot.Console.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ src/EnglishAssistantTelegramBot.Console/Entities/Story.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishAssistantTelegramBot.Console.Entities
+{
+    public class Story : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public int TotalWords { get; set; }
+        public int TotalUniqueWords { get; set; }
+        public string PreviewImage { get; set; }
+        public string SoundFile { get; set; }
+    }
+}
