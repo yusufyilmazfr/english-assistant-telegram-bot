9c14060348f22bb68686ddbe28f1328197acfed8
Yusuf <yusufyilmazfr@gmail.com>
2021-01-14 21:16:41 +0300

    I created Dapper generic repository.

---
+++ src/EnglishAssistantTelegramBot.Console/Repository/Abstract/BaseRepository/IBaseRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EnglishAssistantTelegramBot.Console.Entities;
+
+namespace EnglishAssistantTelegramBot.Console.Repository.Abstract.BaseRepository
+{
+    /// <summary>
+    /// Generic repository interface. It provides same methods to other repositories.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        /// <summary>
+        /// Get TEntity by specified id.
+        /// </summary>
+        /// <param name="id">Unique id.</param>
+        /// <returns></returns>
+        TEntity GetById(int id);
+
+        /// <summary>
+        /// Get TEntity list.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<TEntity> GetList();
+
+        /// <summary>
+        /// Insert new entity to data source.
+        /// </summary>
+        /// <param name="entity">TEntity instance.</param>
+        /// <returns></returns>
+        int Insert(TEntity entity);
+
+        /// <summary>
+        /// Update TEntity from data source.
+        /// </summary>
+        /// <param name="entity">TEntity instance.</param>
+        /// <returns></returns>
+        bool Update(TEntity entity);
+
+        /// <summary>
+        /// Remove TEntity from data source.
+        /// </summary>
+        /// <param name="entity">TEntity instance.</param>
+        /// <returns></returns>
+        bool Delete(TEntity entity);
+
+        /// <summary>
+        /// Get TEntity by specified id.
+        /// </summary>
+        /// <param name="id">Unique id.</param>
+        /// <returns></returns>
+        Task<TEntity> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Get TEntity list.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<TEntity>> GetListAsync();
+
+        /// <summary>
+        /// Insert new entity to data source.
+        /// </summary>
+        /// <param name="entity">TEntity instance.</param>
+        /// <returns></returns>
+        Task<int> InsertAsync(TEntity entity);
+
+        /// <summary>
+        /// Update TEntity from data source.
+        /// </summary>
+        /// <param name="entity">TEntity instance.</param>
+        /// <returns></returns>
+        Task<bool> UpdateAsync(TEntity entity);
+
+        /// <summary>
+        /// Remove TEntity from data source.
+        /// </summary>
+        /// <param name="entity">TEntity instance.</param>
+        /// <returns></returns>
+        Task<bool> DeleteAsync(TEntity entity);
+    }
+}
---
+++ src/EnglishAssistantTelegramBot.Console/Repository/Concrete/Dapper/BaseRepository/DapperBaseRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using EnglishAssistantTelegramBot.Console.Entities;
+using EnglishAssistantTelegramBot.Console.Repository.Abstract.BaseRepository;
+using MySql.Data.MySqlClient;
+
+namespace EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper.BaseRepository
+{
+    public class DapperBaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        // I will move base from here to app settings or in another file.
+        private string connectionString = "Server=localhost; Database=englishreaderassistanttelegrambot; Uid=root; Pwd=12345;";
+
+        public TEntity GetById(int id)
+        {
+            using var connection = new MySqlConnection(connectionString);
+
+            return connection.Get<TEntity>(id);
+        }
+
+        public IEnumerable<TEntity> GetList()
+        {
+            using var connection = new MySqlConnection(connectionString);
+
+            return connection.GetAll<TEntity>();
+        }
+
+        public int Insert(TEntity entity)
+        {
+            using var connection = new MySqlConnection(connectionString);
+
+            return (int)connection.Insert<TEntity>(entity);
+        }
+
+        public bool Update(TEntity entity)
+        {
+            using var connection = new MySqlConnection(connectionString);
+
+            return connection.Update<TEntity>(entity);
+        }
+
+        public bool Delete(TEntity entity)
+        {
+            using var connection = new MySqlConnection(connectionString);
+
+            return connection.Delete<TEntity>(entity);
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            await using var connection = new MySqlConnection(connectionString);
+
+            return await connection.GetAsync<TEntity>(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync()
+        {
+            await using var connection = new MySqlConnection(connectionString);
+
+            return await connection.GetAllAsync<TEntity>();
+        }
+
+        public async Task<int> InsertAsync(TEntity entity)
+        {
+            await using var connection = new MySqlConnection(connectionString);
+
+            return await connection.InsertAsync<TEntity>(entity);
+        }
+
+        public async Task<bool> UpdateAsync(TEntity entity)
+        {
+            await using var connection = new MySqlConnection(connectionString);
+
+            return await connection.UpdateAsync<TEntity>(entity);
+        }
+
+        public async Task<bool> DeleteAsync(TEntity entity)
+        {
+            await using var connection = new MySqlConnection(connectionString);
+
+            return await connection.DeleteAsync<TEntity>(entity);
+        }
+    }
+}
