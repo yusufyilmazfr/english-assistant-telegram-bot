550ec8cf8c19533f5ec3b4453e17e086117b27f1
Yusuf <yusufyilmazfr@gmail.com>
2021-01-14 21:18:35 +0300

    I created new word repository for operation of word entity

---
+++ src/EnglishAssistantTelegramBot.Console/Repository/Abstract/IWordRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EnglishAssistantTelegramBot.Console.Entities;
+using EnglishAssistantTelegramBot.Console.Repository.Abstract.BaseRepository;
+
+namespace EnglishAssistantTelegramBot.Console.Repository.Abstract
+{
+    public interface IWordRepository : IBaseRepository<Word>
+    {
+        /// <summary>
+        /// It returns any word.
+        /// </summary>
+        /// <returns></returns>
+        Task<Word> GetAnyWordAsync();
+    }
+}
---
+++ src/EnglishAssistantTelegramBot.Console/Repository/Concrete/Dapper/DapperWordRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using EnglishAssistantTelegramBot.Console.Entities;
+using EnglishAssistantTelegramBot.Console.Repository.Abstract;
+using EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper.BaseRepository;
+using MySql.Data.MySqlClient;
+
+namespace EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper
+{
+    public class DapperWordRepository : DapperBaseRepository<Word>, IWordRepository
+    {
+        // I will move base from here to app settings or in another file.
+        private string connectionString = "Server=localhost; Database=englishreaderassistanttelegrambot; Uid=root; Pwd=12345;";
+
+        public async Task<Word> GetAnyWordAsync()
+        {
+            const string sql = "SELECT * FROM word ORDER BY RAND() LIMIT 1;";
+
+            await using var connection = new MySqlConnection(connectionString);
+
+            return await connection.QueryFirstOrDefaultAsync<Word>(new CommandDefinition(sql));
+        }
+    }
+}
