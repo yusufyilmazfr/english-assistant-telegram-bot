334ef17f90d42b3c4d3b4383a21f7e2265caf6dd
Yusuf <yusufyilmazfr@gmail.com>
2021-01-14 21:19:20 +0300

    I created story generic repository class.

---
+++ src/EnglishAssistantTelegramBot.Console/Repository/Concrete/Dapper/DapperStoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnglishAssistantTelegramBot.Console.Entities;
+using EnglishAssistantTelegramBot.Console.Repository.Abstract;
+using EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper.BaseRepository;
+
+namespace EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper
+{
+    public class DapperStoryRepository : DapperBaseRepository<Story>
+    {
+
+    }
+}
