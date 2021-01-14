3e641c7b098169adedd36719ceae6bc7fa258a9a
Yusuf <yusufyilmazfr@gmail.com>
2021-01-14 21:20:30 +0300

    ITelegramClient provides to access telegram bot instance.

---
+++ src/EnglishAssistantTelegramBot.Console/Client/ITelegramClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+
+namespace EnglishAssistantTelegramBot.Console.Client
+{
+    public interface ITelegramClient
+    {
+        ITelegramBotClient GetInstance();
+    }
+}
---
+++ src/EnglishAssistantTelegramBot.Console/Client/TelegramClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+
+namespace EnglishAssistantTelegramBot.Console.Client
+{
+    public class TelegramClient : ITelegramClient
+    {
+        private static ITelegramBotClient _telegramBotClient;
+        private static readonly object _lockObject = new object();
+
+        public ITelegramBotClient GetInstance()
+        {
+            if (_telegramBotClient == null)
+            {
+                lock (_lockObject)
+                {
+                    if (_telegramBotClient == null)
+                    {
+                        _telegramBotClient = new TelegramBotClient("KEY");
+                    }
+                }
+            }
+
+            return _telegramBotClient;
+        }
+    }
+}
