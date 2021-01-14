ff738e1103d2a0f139996765dea631ceac6f418c
Yusuf <yusufyilmazfr@gmail.com>
2021-01-14 21:26:04 +0300

    TelegramCommandFactory provides factory design pattern for matching command

---
+++ src/EnglishAssistantTelegramBot.Console/CommandFactory/ITelegramCommandFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnglishAssistantTelegramBot.Console.Commands.Abstract;
+using Telegram.Bot.Types;
+
+namespace EnglishAssistantTelegramBot.Console.CommandFactory
+{
+    public interface ITelegramCommandFactory
+    {
+        ICommand CreateCommand(Message command);
+    }
+}
---
+++ src/EnglishAssistantTelegramBot.Console/CommandFactory/TelegramCommandFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnglishAssistantTelegramBot.Console.Client;
+using EnglishAssistantTelegramBot.Console.Commands.Abstract;
+using EnglishAssistantTelegramBot.Console.Commands.Concrete;
+using EnglishAssistantTelegramBot.Console.Repository.Abstract;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace EnglishAssistantTelegramBot.Console.CommandFactory
+{
+    public class TelegramCommandFactory : ITelegramCommandFactory
+    {
+        private ITelegramBotClient TelegramBotClient { get; }
+        private readonly IWordRepository _wordRepository;
+
+        public TelegramCommandFactory(ITelegramClient telegramClient, IWordRepository wordRepository)
+        {
+            _wordRepository = wordRepository;
+            TelegramBotClient = telegramClient.GetInstance();
+        }
+
+        public ICommand CreateCommand(Message command)
+        {
+            switch (command.Text)
+            {
+                case "/start":
+                    return new ShowCommand(TelegramBotClient);
+                case "/sendnewstory":
+                    return new SendStoryCommand(TelegramBotClient);
+                case "/sendnewword":
+                    return new SendNewWordCommand(_wordRepository, TelegramBotClient);
+                default:
+                    return new ShowCommand(TelegramBotClient);
+            }
+        }
+    }
+}
