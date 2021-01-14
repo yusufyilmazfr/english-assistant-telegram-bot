99a5f34d493343bbb4bf86512c9ae7fa0db04ac6
Yusuf <yusufyilmazfr@gmail.com>
2021-01-14 21:26:31 +0300

    Basically client code. It works.

---
+++ src/EnglishAssistantTelegramBot.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using EnglishAssistantTelegramBot.Console.Client;
+using EnglishAssistantTelegramBot.Console.CommandFactory;
+using EnglishAssistantTelegramBot.Console.Repository.Abstract;
+using EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+
+namespace EnglishAssistantTelegramBot.Console
+{
+    class Program
+    {
+        public static ITelegramCommandFactory TelegramCommandFactory;
+
+        static void Main(string[] args)
+        {
+            ITelegramClient telegramClient = new TelegramClient();
+            ITelegramBotClient telegramBotClient = telegramClient.GetInstance();
+            IWordRepository wordRepository = new DapperWordRepository();
+
+            TelegramCommandFactory = new TelegramCommandFactory(telegramClient, wordRepository);
+
+            telegramBotClient.OnMessage += TelegramBotClientOnOnMessage;
+            telegramBotClient.StartReceiving();
+
+            System.Console.WriteLine("Please press key to exit");
+
+            System.Console.Read();
+
+            telegramBotClient.StopReceiving();
+        }
+
+        private static async void TelegramBotClientOnOnMessage(object sender, MessageEventArgs e)
+        {
+            if (e?.Message == null)
+            {
+                return;
+            }
+
+            await TelegramCommandFactory.CreateCommand(e?.Message).ExecuteAsync(e.Message);
+        }
+    }
+}
