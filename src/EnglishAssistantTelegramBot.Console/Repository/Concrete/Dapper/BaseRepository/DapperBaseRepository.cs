2e4362419bb86d7b958716cc0df54becc020fefe
Yusuf <yusufyilmazfr@gmail.com>
2021-01-15 20:09:22 +0300

    I removed magic string in repositories. I use IConfigurationContext instance for connection string.

--- src/EnglishAssistantTelegramBot.Console/Repository/Concrete/Dapper/BaseRepository/DapperBaseRepository.cs
+++ src/EnglishAssistantTelegramBot.Console/Repository/Concrete/Dapper/BaseRepository/DapperBaseRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper.Contrib.Extensions;
+using EnglishAssistantTelegramBot.Console.Configuration.Context;
 using EnglishAssistantTelegramBot.Console.Entities;
 using EnglishAssistantTelegramBot.Console.Repository.Abstract.BaseRepository;
 using MySql.Data.MySqlClient;
@@ -11,75 +12,79 @@ namespace EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper.BaseRep
 {
     public class DapperBaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
     {
-        // I will move base from here to app settings or in another file.
-        private string connectionString = "Server=localhost; Database=englishreaderassistanttelegrambot; Uid=root; Pwd=12345;";
+        private readonly string _connectionString;
+
+        public DapperBaseRepository(IConfigurationContext configurationContext)
+        {
+            _connectionString = configurationContext.MySQLConnectionString;
+        }
 
         public TEntity GetById(int id)
         {
-            using var connection = new MySqlConnection(connectionString);
+            using var connection = new MySqlConnection(_connectionString);
 
             return connection.Get<TEntity>(id);
         }
 
         public IEnumerable<TEntity> GetList()
         {
-            using var connection = new MySqlConnection(connectionString);
+            using var connection = new MySqlConnection(_connectionString);
 
             return connection.GetAll<TEntity>();
         }
 
         public int Insert(TEntity entity)
         {
-            using var connection = new MySqlConnection(connectionString);
+            using var connection = new MySqlConnection(_connectionString);
 
             return (int)connection.Insert<TEntity>(entity);
         }
 
         public bool Update(TEntity entity)
         {
-            using var connection = new MySqlConnection(connectionString);
+            using var connection = new MySqlConnection(_connectionString);
 
             return connection.Update<TEntity>(entity);
         }
 
         public bool Delete(TEntity entity)
         {
-            using var connection = new MySqlConnection(connectionString);
+            using var connection = new MySqlConnection(_connectionString);
 
             return connection.Delete<TEntity>(entity);
         }
 
         public async Task<TEntity> GetByIdAsync(int id)
         {
-            await using var connection = new MySqlConnection(connectionString);
+            await using var connection = new MySqlConnection(_connectionString);
 
             return await connection.GetAsync<TEntity>(id);
         }
 
         public async Task<IEnumerable<TEntity>> GetListAsync()
         {
-            await using var connection = new MySqlConnection(connectionString);
+            await using var connection = new MySqlConnection(_connectionString);
 
             return await connection.GetAllAsync<TEntity>();
         }
 
         public async Task<int> InsertAsync(TEntity entity)
         {
-            await using var connection = new MySqlConnection(connectionString);
+            await using var connection = new MySqlConnection(_connectionString);
 
             return await connection.InsertAsync<TEntity>(entity);
         }
 
         public async Task<bool> UpdateAsync(TEntity entity)
         {
-            await using var connection = new MySqlConnection(connectionString);
+            await using var connection = new MySqlConnection(_connectionString);
 
             return await connection.UpdateAsync<TEntity>(entity);
         }
 
         public async Task<bool> DeleteAsync(TEntity entity)
         {
-            await using var connection = new MySqlConnection(connectionString);
+            await using var connection = new MySqlConnection(_connectionString);
 
             return await connection.DeleteAsync<TEntity>(entity);
         }
--- src/EnglishAssistantTelegramBot.Console/Repository/Concrete/Dapper/DapperVolunteerPageRepository.cs
+++ src/EnglishAssistantTelegramBot.Console/Repository/Concrete/Dapper/DapperVolunteerPageRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
+using EnglishAssistantTelegramBot.Console.Configuration.Context;
 using EnglishAssistantTelegramBot.Console.Entities;
 using EnglishAssistantTelegramBot.Console.Repository.Abstract;
 using EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper.BaseRepository;
@@ -13,14 +14,18 @@ namespace EnglishAssistantTelegramBot.Console.Repository.Concrete.Dapper
     public class DapperVolunteerPageRepository : DapperBaseRepository<VolunteerPage>, IVolunteerPageRepository
     {
         // I will move base from here to app settings or in another file.
-        private string connectionString = "Server=localhost; Database=englishreaderassistanttelegrambot; Uid=root; Pwd=12345;";
+        private readonly string _connectionString;
 
+        public DapperVolunteerPageRepository(IConfigurationContext configurationContext) : base(configurationContext)
+        {
+            _connectionString = configurationContext.MySQLConnectionString;
+        }
 
         public async Task<VolunteerPage> GetAnyVolunteerPageAsync()
         {
             const string sql = "SELECT * FROM volunteerpage ORDER BY RAND() LIMIT 1;";
 
-            await using var connection = new MySqlConnection(connectionString);
+            await using var connection = new MySqlConnection(_connectionString);
 
             return await connection.QueryFirstOrDefaultAsync<VolunteerPage>(new CommandDefinition(sql));
         }
