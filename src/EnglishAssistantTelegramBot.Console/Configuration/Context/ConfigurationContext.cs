9306902f7710a4f9794c640ac30c9f61aa4826d2
Yusuf <yusufyilmazfr@gmail.com>
2021-01-15 20:08:11 +0300

    Appsettings propertied defined in context

---
+++ src/EnglishAssistantTelegramBot.Console/Configuration/Context/ConfigurationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnglishAssistantTelegramBot.Console.Configuration.Environment;
+
+namespace EnglishAssistantTelegramBot.Console.Configuration.Context
+{
+    public class ConfigurationContext : IConfigurationContext
+    {
+        public string MySQLConnectionString { get; set; }
+        public string TelegramBotKey { get; set; }
+
+        public ConfigurationContext(IEnvironmentService environmentService)
+        {
+            MySQLConnectionString = environmentService.Configuration["MySQLConnectionString"];
+            TelegramBotKey = environmentService.Configuration["TelegramBotKey"];
+        }
+    }
+}
---
+++ src/EnglishAssistantTelegramBot.Console/Configuration/Context/IConfigurationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnglishAssistantTelegramBot.Console.Configuration.Context
+{
+    /// <summary>
+    /// This interface provides value of configuration properties.
+    /// </summary>
+    public interface IConfigurationContext
+    {
+        public string MySQLConnectionString { get; set; }
+        public string TelegramBotKey { get; set; }
+    }
+}
