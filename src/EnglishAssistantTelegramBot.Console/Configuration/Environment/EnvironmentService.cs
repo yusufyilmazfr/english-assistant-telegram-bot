10331151d0fc18687091e6fc20d6b128ed9240ff
Yusuf <yusufyilmazfr@gmail.com>
2021-01-15 20:07:43 +0300

    Environment service provides accessing to configuration

---
+++ src/EnglishAssistantTelegramBot.Console/Configuration/Environment/EnvironmentService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace EnglishAssistantTelegramBot.Console.Configuration.Environment
+{
+    public class EnvironmentService : IEnvironmentService
+    {
+        private static IConfiguration _configuration;
+
+        public EnvironmentService()
+        {
+            SetConfiguration();
+        }
+
+        public IConfiguration Configuration => _configuration;
+
+        private void SetConfiguration()
+        {
+            if (_configuration == null)
+            {
+                var configurationBuilder = new ConfigurationBuilder();
+
+                var appsettingsFileName = $"Configuration/appsettings.json";
+
+                _configuration = configurationBuilder.SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile(appsettingsFileName, optional: false, reloadOnChange: true)
+                    .Build();
+            }
+        }
+    }
+}
