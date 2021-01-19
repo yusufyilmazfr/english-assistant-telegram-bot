778ecdc439dc35eb72cd9d2d1268d653a6f25229
Yusuf <yusufyilmazfr@gmail.com>
2021-01-19 23:14:15 +0300

    defined explicit key

--- src/EnglishAssistantTelegramBot.Console/Entities/BaseEntity.cs
+++ src/EnglishAssistantTelegramBot.Console/Entities/BaseEntity.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Dapper.Contrib.Extensions;
 
 namespace EnglishAssistantTelegramBot.Console.Entities
 {
     public class BaseEntity
     {
+        [ExplicitKey]
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
